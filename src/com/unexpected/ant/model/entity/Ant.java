bfba6e84128b62e4489cd29a06c9be43269153a5
Keskeny <keskeny666@gmail.com>
2013-04-23 22:57:41 +0200

    createSmell

--- src/com/unexpected/ant/model/entity/Ant.java
+++ src/com/unexpected/ant/model/entity/Ant.java
@@ -34,8 +34,10 @@ public class Ant extends MovingEntity {
 
 	@Override
 	public void action(long tickCount) {
-		if (tickCount % 10 == 0)
+		if (tickCount % 10 == 0) {
+            createSmell();
 			move();
+        }
 	}
 
 	/**
