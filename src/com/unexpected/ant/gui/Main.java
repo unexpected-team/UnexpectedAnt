0bd47c27ecba003c640f7f397e298ace76df2d24
Keskeny <keskeny666@gmail.com>
2013-05-14 23:19:19 +0200

    komment

--- src/com/unexpected/ant/gui/Main.java
+++ src/com/unexpected/ant/gui/Main.java
@@ -10,16 +10,24 @@ import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
+        /**
+         * Set random position for entity
+         */
         Integer[] num = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49};
 
-        // As the Collections.shuffle method need a list for the parameter we
-        // convert our array into List using the Arrays class.
+        /**
+         * As the Collections.shuffle method need a list for the parameter we convert our array into List using the Arrays class.
+         */
         List<Integer> list = Arrays.asList(num);
 
-        // Here we just simply used the shuffle method of Collections class
-        // to shuffle out defined array.
+        /**
+         *   Here we just simply used the shuffle method of Collections class to shuffle out defined array.
+         */
         Collections.shuffle(list);
 
+        /**
+         * Build GameField
+         */
 		GameEngine.INSTANCE.reset();
 		GameEngine.INSTANCE.getGameField().getCells().get(list.get(0)).addEntity(new Food());
 		GameEngine.INSTANCE.getGameField().getCells().get(list.get(1)).addEntity(new AntHill());
